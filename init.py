6fc995fbea40c1d030346073c8c5f888be17af45
Elijah Shope <eshope7489@gmail.com>
2019-11-15 14:52:16 +0000

    working on players

--- init.py
+++ init.py
@@ -7,12 +7,14 @@ money = 500
 suit = ["Hearts", "Diamands", "Clubs", "Spades"]
 Number = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Jack", "Queen", "King"]
 Deck = []
-def genHand():
+hands = []
+def genHand(hands):
 	counter = 0
 	for i in suit:
 		for j in Number:
 			Deck.append(j + " of " + i)
-			
-	return Deck
+	print("How many people do you want to play with?")
+	players = input()
+	print(players)
 
-print(genHand())
+genHand(hands)
