e2d25d7d766fd8d46851c4aacbcb3962cb067826
Elijah Shope <22753264+TNTUSER55@users.noreply.github.com>
2019-11-20 17:42:46 +0000

    working on rounds

--- init.py
+++ init.py
@@ -8,13 +8,19 @@ suit = ["Hearts", "Diamands", "Clubs", "Spades"]
 Number = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Jack", "Queen", "King"]
 Deck = []
 hands = []
-def genHand(hands):
+
+
+
+def genDeck(Deck):
 	counter = 0
 	for i in suit:
 		for j in Number:
 			Deck.append(j + " of " + i)
-	print("How many people do you want to play with?")
-	players = input()
-	print(players)
 
-genHand(hands)
+def round(Deck, money):
+	bet = input("How much do you bet" + " You have " + str(money) + "?")
+	money -= bet
+
+
+genDeck(Deck)
+round(Deck, money)
